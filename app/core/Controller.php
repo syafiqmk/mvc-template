aa0415f923eed66d13a4993c00d083fc11c78680
syafiqmk <syafiqmuh.k@gmail.com>
2022-06-07 14:06:30 +0800

    fix model method

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -10,6 +10,14 @@
         //method model
         public function model($model) {
             require_once "../app/models/".$model.".php";
+            return new $model;
+        }
+
+        //check login
+        public function cekLogin() {
+            if(empty($_SESSION['login'])) {
+                header("location:".BASE_URL."/login");
+            }
         }
     }
 ?>
